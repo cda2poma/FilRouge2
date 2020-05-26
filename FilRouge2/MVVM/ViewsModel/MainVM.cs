d953ba08df9f127465f1416da8f1d8507c3f2b3f
cda2poma <alban.pomarede@2isa.org>
2020-05-26 09:02:51 +0200

    Ajoutez des fichiers projet.

---
+++ FilRouge2/MVVM/ViewsModel/MainVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilRouge2
+{
+    class MainVM : ViewModelBase
+    {
+        private string _offreDesc;
+
+        public string OffreDesc
+        {
+            get { return _offreDesc; }
+            set
+            {
+                _offreDesc = value;
+                RaisepropertyChanged();
+            }
+        }
+    }
+}
---
+++ FilRouge2/MVVM/ViewsModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace FilRouge2
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisepropertyChanged([CallerMemberName]string propertyName = "")
+        { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); }
+    }
+}
